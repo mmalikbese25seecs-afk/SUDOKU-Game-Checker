6456282ebe08086f498f0875d2dfaf53930dd558
Muhammad Salman Ali Malik <mmalik.bese25seecs@seecs.edu.pk>
2025-11-25 21:48:22 +0500

    Implement rowcheck function for Sudoku validation
    Added a function to check for duplicate values in a Sudoku row.

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,8 +3,26 @@
 #include <ctime>
 using namespace std;
 
+   bool rowcheck(int array[9][9],int lim, int n);
+
+//-------------------------------------MAIN FUNCTION---------------------------------------
 int main(){
 
   system("pause");
   return 0;
+}
+//------------------------------------------------------------------------------------------
+bool rowcheck(int array[9][9],int lim, int n){
+    for(int j=0; j<lim; j++){      
+
+    if(array[n][j] == 0){
+	  continue;  ??If array
+	 }
+    for(int k=j+1 ; k<lim; k++){
+     if(array[n][j]==array[n][k]){
+      return true;
+            }
+        }
+    }
+      return false;
 }
